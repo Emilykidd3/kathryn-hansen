59df94723b345ff7b98470f5c68df07008a3f85a
emilykidd3 <e.kidd61@yahoo.com>
2021-05-13 18:24:54 -0700

    added styled nav bar

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,38 +1,44 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
 function Nav() {
-    return(
-        <header>
-            <div className="nav-wrapper">
-                <Link to='/'>
-                    Home
-                </Link>
-                <Link to='/about'>
-                    About
-                </Link>
-                <Link to='/gallery'>
-                    Gallery
-                </Link>
-            </div>
-            <h1>
-                <Link to='/'>
-                    Kathryn Hansen
-                </Link>
-            </h1>
-            <div>
-                <Link to='/contact'>
-                    Contact
-                </Link>
-                <Link to='/events'>
-                    Events
-                </Link>
-                <Link to='/'>
-                    Shop
-                </Link>
-            </div>
-        </header>
-    );
+  return (
+    <header>
+      <nav className="">
+        <div className="nav-wrapper container white-text">
+          <ul className="right">
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/gallery">Gallery</Link>
+            </li>
+          </ul>
+          <Link
+            to="/"
+            className="brand-logo center"
+            style={{ fontSize: "150%" }}
+          >
+            Kathryn Hansen
+          </Link>
+          <ul className="left">
+            <li>
+              <Link to="/contact">Contact</Link>
+            </li>
+            <li>
+              <Link to="/events">Events</Link>
+            </li>
+            <li>
+              <Link to="/">Shop</Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </header>
+  );
 }
 
 export default Nav;
