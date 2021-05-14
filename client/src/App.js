f0322cffc3b6410eb13aea52cd2abc4c2229a8f1
emilykidd3 <e.kidd61@yahoo.com>
2021-05-13 17:19:59 -0700

    added nav routes and pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,54 @@
-import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Route, Router, Switch } from "react-router-dom";
+import { ApolloProvider } from "@apollo/react-hooks";
+import ApolloClient from "apollo-boost";
 
-import Nav from './components/Nav';
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Gallery from "./pages/Gallery";
+import Contact from "./pages/Contact";
+import Events from "./pages/Events";
+import NoMatch from "./pages/NoMatch";
+
+import Nav from "./components/Nav";
+
+// import { StoreProvider } from './utils/GlobalState';
+
+// are we / should we be using a token
+const client = new ApolloClient(
+  { uri: '/graphql' }
+  );
+  
+//   {
+//   request: (operation) => {
+//     const token = localStorage.getItem("id_token");
+//     operation.setContext({
+//       headers: {
+//         authorization: token ? `Bearer ${token}` : "",
+//       },
+//     });
+//   },
+//   uri: "/graphql",
+// });
 
 function App() {
   return (
-    <div>
+    <ApolloProvider client={client}>
       <Router>
-        <Nav />
+        <div>
+          <Nav />
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/about" component={About} />
+            <Route exact path="/gallery" component={Gallery} />
+            <Route exact path="/contact" component={Contact} />
+            <Route exact path="/events" component={Events} />
+            <Route exact path="/" />
+            <Route component={NoMatch} />
+          </Switch>
+        </div>
       </Router>
-    </div>
+    </ApolloProvider>
   );
 }
 
--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -4,11 +4,33 @@ import { Link } from 'react-router-dom';
 function Nav() {
     return(
         <header>
+            <div className="nav-wrapper">
+                <Link to='/'>
+                    Home
+                </Link>
+                <Link to='/about'>
+                    About
+                </Link>
+                <Link to='/gallery'>
+                    Gallery
+                </Link>
+            </div>
             <h1>
                 <Link to='/'>
                     Kathryn Hansen
                 </Link>
             </h1>
+            <div>
+                <Link to='/contact'>
+                    Contact
+                </Link>
+                <Link to='/events'>
+                    Events
+                </Link>
+                <Link to='/'>
+                    Shop
+                </Link>
+            </div>
         </header>
     );
 }
