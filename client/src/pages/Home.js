f7c7ce18f6a6c617efabfaec7e96b7d8c9c11b9e
emilykidd3 <e.kidd61@yahoo.com>
2021-05-15 14:10:43 -0700

    switched homepage image to background image and finished footer

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,15 +1,30 @@
-import React from 'react';
-import {ViewHolder} from '../components/ViewHolder';
+import React from "react";
+import { ViewHolder } from "../components/ViewHolder";
 
 const Home = () => {
-    return(
-        <ViewHolder>
-            <img src={`${process.env.PUBLIC_URL}/images/im-all-ears.jpg`} alt="elephant" style={{width: "100%"}}></img>
-            <div className="" style={{ position: "absolute", top: "90%", left: "50%", transform: "translate(-50%, -50%)", textAlign: "center" }}>
-                text lorem ipsum words text lorem ipsum words text lorem ipsum words text lorem ipsum words text lorem ipsum words text lorem ipsum words text lorem ipsum words text lorem ipsum words text lorem ipsum words 
-            </div>
-        </ViewHolder>
-    )
-}
+  return (
+    <ViewHolder
+      style={{
+        backgroundImage: `url(${process.env.PUBLIC_URL}/images/im-all-ears.jpg)`,
+        width: "100%",
+        backgroundSize: "cover",
+      }}
+    >
+      <div
+        style={{
+          position: "absolute",
+          top: "90%",
+          left: "50%",
+          transform: "translate(-50%, -50%)",
+          textAlign: "center",
+        }}
+      >
+        text lorem ipsum words text lorem ipsum words text lorem ipsum words
+        text lorem ipsum words text lorem ipsum words text lorem ipsum words
+        text lorem ipsum words text lorem ipsum words text lorem ipsum words
+      </div>
+    </ViewHolder>
+  );
+};
 
 export default Home;
