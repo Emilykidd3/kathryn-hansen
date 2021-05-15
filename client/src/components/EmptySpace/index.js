85cffb2ace78017908a9da76846345d5d527d26a
emilykidd3 <e.kidd61@yahoo.com>
2021-05-14 20:46:43 -0700

    finished navigation bar

---
+++ client/src/components/EmptySpace/index.js
@@ -0,0 +1,6 @@
+import styled from 'styled-components';
+
+export const EmptySpace = styled.div`
+    height: 30px;
+    width: 100vw;
+`
--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,51 +1,53 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import {
-  Collapse,
-  Navbar,
-  NavbarToggler,
-  NavbarBrand,
-  Nav,
-  NavItem,
-  UncontrolledDropdown,
-  DropdownToggle,
-  DropdownMenu,
-  DropdownItem,
-  NavbarText
-} from 'reactstrap';
+import { Navbar, NavbarBrand, Nav, NavLink, NavItem } from "reactstrap";
+import styled from "styled-components";
+
+const StyledLink = styled(Link)`
+  text-decoration: none;
+  color: black;
+`;
+
 function Navigation() {
   return (
-      <Navbar>
-        <div>
-          <Nav>
-            <NavItem>
-              <Link to="/">Home</Link>
-            </NavItem>
-            <NavItem>
-              <Link to="/about">About</Link>
-            </NavItem>
-            <NavItem>
-              <Link to="/gallery">Gallery</Link>
-            </NavItem>
-          </Nav>
-          <Link
-            to="/"
+    <Navbar light expand="md" style={{ background: "transparent", position: "absolute"}} fixed="top">
+      <Nav className="container-fluid" navbar style={{ width: "20%" }}>
+        <NavItem className="ml-auto">
+          <StyledLink to="/">HOME</StyledLink>
+        </NavItem>
+        <NavItem className="ml-auto">
+          <StyledLink to="/about">ABOUT</StyledLink>
+        </NavItem>
+        <NavItem className="ml-auto">
+          <StyledLink to="/gallery">GALLERY</StyledLink>
+        </NavItem>
+      </Nav>
+
+      <NavbarBrand style={{ marginLeft: "10px", marginRight: "10px", fontSize: "40px" }}>
+        KATHRYN HANSEN
+      </NavbarBrand>
+
+      <Nav className="container-fluid" navbar style={{ width: "20%" }}>
+        <NavItem className="mr-auto">
+          <StyledLink to="/contact">CONTACT</StyledLink>
+        </NavItem>
+        <NavItem className="mr-auto">
+          <StyledLink to="/events">EVENTS</StyledLink>
+        </NavItem>
+        <NavItem className="mr-auto">
+          <NavLink
+            href="https://fineartamerica.com/profiles/kathryn-hansen"
+            style={{ color: "black" }}
           >
-            Kathryn Hansen
-          </Link>
-          <Nav>
-            <NavItem>
-              <Link to="/contact">Contact</Link>
-            </NavItem>
-            <NavItem>
-              <Link to="/events">Events</Link>
-            </NavItem>
-            <NavItem>
-              <Link to="/">Shop</Link>
-            </NavItem>
-          </Nav>
-        </div>
-      </Navbar>
+            SHOP
+          </NavLink>
+        </NavItem>
+      </Nav>
+    </Navbar>
   );
 }
 export default Navigation;
+
+// each component should have 100vh style={{height: "100vh"}}
+
+// 
