e8f157c616c532c461bab56584856eba074112e0
Priya Macpherson <priyamacpherson@Priyas-MacBook-Pro.local>
2021-05-16 11:22:38 -0700

    created AddEvent and AddPhoto pages

--- server/config/connection.js
+++ server/config/connection.js
@@ -10,4 +10,11 @@ mongoose.connect(
   }
 );
 
+const cloudinary = require("cloudinary");
+cloudinary.config({
+  cloud_name: "dej8kpmbw",
+  api_key: "279398537595666",
+  api_secret: "V3IiCtzagihOl8lbX4zFAX1FaT8",
+});
+
 module.exports = mongoose.connection;
