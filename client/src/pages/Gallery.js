13149e0195916942bdbf2899281a252c178018db
emilykidd3 <e.kidd61@yahoo.com>
2021-05-23 13:20:49 -0700

    made gallery mobile friendly

--- client/src/pages/Gallery.js
+++ client/src/pages/Gallery.js
@@ -15,6 +15,9 @@ import {
 import { EmptySpace } from "../components/EmptySpace";
 import { GrayDivider } from "../components/GrayDivider";
 import { QUERY_ALL_GALLERY } from "../utils/queries";
+import {MobileEmptySpace} from '../components/MobileEmptySpace';
+
+const phone = 768
 
 //map inside
 const Gallery = () => {
@@ -37,6 +40,64 @@ const Gallery = () => {
   console.log(data.galleries[1]);
 
   return (
+    phone >= window.innerWidth ? 
+    <div>
+      <MobileEmptySpace />
+      <h2
+        style={{
+          textAlign: "center",
+          fontWeight: "200",
+          marginBottom: "40px",
+        }}
+      >
+        GALLERY
+      </h2>
+      <GrayDivider />
+      <Container>
+        <Row>
+          <CardGroup style={{ width: "90%", margin: "0 auto" }}>
+            <Col xs="12">
+            {data.galleries.length > 0
+              ? data.galleries.map((el) => (
+                    <Card key={el._id} style={{ marginLeft: "10px", marginRight: "10px", marginBottom: "10px" }}>
+                      <CardImg
+                        top
+                        width="100%"
+                        src={el.image}
+                        alt="Card image cap"
+                      />
+                      <CardBody>
+                        <CardTitle tag="h5" style={{ fontWeight: "lighter" }}>
+                          {el.title}
+                        </CardTitle>
+                        <CardSubtitle
+                          tag="h6"
+                          className="mb-2 text-muted"
+                          style={{ fontWeight: "lighter" }}
+                        >
+                          Colored Pencil on Paper
+                        </CardSubtitle>
+                        {/* if link is null, put contact artist */}
+                        <a href={el.link} target="_blank">
+                          <Button
+                            style={{
+                              backgroundColor: "#A66D60",
+                              fontWeight: "lighter",
+                            }}
+                          >
+                            Purchase Prints
+                          </Button>
+                        </a>
+                      </CardBody>
+                    </Card>
+                ))
+                : ""}
+              </Col>
+          </CardGroup>
+        </Row>
+      </Container>
+    </div>
+     :
     <div>
       <EmptySpace />
       <h2
